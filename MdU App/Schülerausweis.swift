14a7c1eeb456ebc31410b08ba58a1ea17df24cae
Noah R <nr@krasscrash.de>
2025-02-24 17:36:11 +0100

    Schülerausweis first commit

---
+++ MdU App/Schülerausweis.swift
@@ -0,0 +1,23 @@
+//
+//  Schülerausweis.swift
+//  MdU App
+//
+//  Created by Noah R on 24.02.25.
+//
+
+import SwiftUI
+
+struct Schülerausweis: View {
+    var body: some View {
+        Text("Schülerausweis View")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct Schülerausweis_Previews: PreviewProvider {
+    static var previews: some View {
+        Schülerausweis()
+    }
+}
+
